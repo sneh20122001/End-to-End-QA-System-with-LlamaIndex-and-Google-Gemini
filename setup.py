baf50bdea19b60696aec22ea56131414170cedde
Sneh Patel <95904935+sneh20122001@users.noreply.github.com>
2025-03-21 12:28:38 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='QA_APPLICATION',
+    version='0.0.1',
+    author='SNEH PATEL',
+    author_email='snehpatel19@gnu.ac.in',
+    packages=find_packages(),
+    install_requires = []
+)
